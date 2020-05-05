7bc68ab7593795c056d89ba4d95a20f2ba64c351
Andreas Thomas <dev@chronark.com>
2020-05-05 17:03:47 +0200

    [WIP] Remove supertest

--- src/srv/api/routes.test.ts
+++ src/srv/api/routes.test.ts
@@ -1,20 +1,10 @@
-import supertest from "supertest"
-import app from "./app"
 import fetch from "node-fetch"
+import { handler } from "./routes"
+import express from "express"
 import { mocked } from "ts-jest/utils"
-
-jest.mock("node-fetch", () => {
-  return jest.fn()
-})
-
-const req = supertest(app)
+jest.mock("node-fetch", () => jest.fn())
 
 describe("Test the API endpoint", () => {
-  it("should reply to GET with 404", async () => {
-    const response = await req.get("/")
-    expect(response.status).toBe(404)
-  })
-
   it("should reply to POST with 200 and return capitalized json message", async () => {
     mocked(fetch).mockImplementation(
       (): Promise<any> => {
@@ -26,10 +16,25 @@ describe("Test the API endpoint", () => {
       },
     )
 
-    const response = await req.post("/").send({ text: "hello" }).set("Accept", "application/json")
+    const mockExpressReq = (): express.Request => {
+      const req: express.Request = express.request
+      req.body = JSON.stringify({ text: "hello world" })
+      return req
+    }
+
+    const mockExpressRes = (): express.Response => {
+      const res: express.Response = express.response
+      res.status = () => res
+      res.json = () => res
+      return res
+    }
+
+    const response = mockExpressRes()
+
+    await handler(mockExpressReq(), response)
 
     expect(fetch).toHaveBeenCalledTimes(1)
     expect(response.status).toBe(200)
-    expect(response.body).toEqual(JSON.stringify({ capitalizedText: "HELLO" }))
+    expect(response.json).toEqual(JSON.stringify({ capitalizedText: "HELLO" }))
   })
 })
