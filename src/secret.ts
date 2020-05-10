4379858fea83a0e666e37350827b52b92de55638
JuliaSte <julia_herford@hotmail.de>
2020-05-10 18:33:06 +0200

    This commit extends the secret to a 32 byte long random string

--- src/secret.ts
+++ src/secret.ts
@@ -5,7 +5,7 @@ import crypto from "crypto-random-string"
  * @returns JS Object containing secret and hash
  */
 export function getSecret(): { secret: string; hash: string } {
-  const secret = crypto({ length: 10, type: "base64" })
+  const secret = crypto({ length: 32, type: "base64" })
   const hash = SHA256(secret).toString()
   return {
     secret,
