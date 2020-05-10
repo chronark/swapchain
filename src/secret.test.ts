5814680e5e5924e053f70d1d945eca7102e89784
Mads Jordt <madsjordt@MacBook-Pro.local>
2020-05-10 14:33:58 +0200

    Add test description

--- src/secret.test.ts
+++ src/secret.test.ts
@@ -1,7 +1,7 @@
 import { SHA256 } from "crypto-js"
 import { getSecret } from "./secret"
 
-test("XXX", () => {
+test("should hash the secret to same hash", () => {
   const secretObj = getSecret()
 
   expect(secretObj.hash).toBe(SHA256(secretObj.secret).toString())
