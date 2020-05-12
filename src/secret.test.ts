a79db69430466ae944227d99bc7c1f4d0ae11642
webersni <nicolas.webersinke@outlook.com>
2020-05-12 10:24:10 +0200

    Add secret length test case

--- src/secret.test.ts
+++ src/secret.test.ts
@@ -1,8 +1,13 @@
 import { SHA256 } from "crypto-js"
 import { getSecret } from "./secret"
 
-test("should hash the secret to same hash", () => {
-  const secretObj = getSecret()
-
-  expect(secretObj.hash).toBe(SHA256(secretObj.secret).toString())
+describe("Secret generator", () => {
+  const length = 32
+  const secretObj = getSecret(length)
+  it("should hash the secret to same hash", () => {
+    expect(secretObj.hash).toBe(SHA256(secretObj.secret).toString())
+  })
+  it("should return a secret with the requested length", () => {
+    expect(secretObj.secret.length).toBe(length)
+  })
 })
