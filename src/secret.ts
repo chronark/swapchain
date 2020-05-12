132b2f6b05039266310f52f9af66a78634979391
webersni <nicolas.webersinke@outlook.com>
2020-05-12 10:23:19 +0200

    Add interface for secret and adjustable secret length

--- src/secret.ts
+++ src/secret.ts
@@ -2,12 +2,22 @@ import { SHA256 } from "crypto-js"
 import crypto from "crypto-random-string"
 
 /**
+ * An interface for a secret/hash pair
+ */
+export interface Secret {
+  secret: string
+  hash: string
+}
+
+/**
  * A function for hashing a crypto random string.
  *
- * @returns JS Object containing secret and hash
+ * @param length The length of the requested secret
+ * @returns An Secret interface with a secret/hash pair
+ *
  */
-export function getSecret(): { secret: string; hash: string } {
-  const secret = crypto({ length: 32, type: "base64" })
+export function getSecret(length: number): Secret {
+  const secret = crypto({ length, type: "base64" })
   const hash = SHA256(secret).toString()
   return {
     secret,
