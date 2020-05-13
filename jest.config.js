93d291e5b355ed6721893f1527e734da7911924b
Andreas Thomas <dev@chronark.com>
2020-05-13 18:16:28 +0200

    Refactor directory structure
    Created the following directories and moved all files to their correct
    destination.
    
    - build (Note that we are not building to ./dist)
    	Scripts and tools for building, packaging and deployment.
    - pkg
    	Shared code that multiple services depend upon.
    - srv
    	Contains a subdirectory for each service. Services must be
    	independent of each other and communicated using a predefined
    	 API.
    - test
    	For mocks and other common test setup or integration tests.
    - tmp
    	A place for our temporary POC code.

--- jest.config.js
+++ jest.config.js
@@ -150,7 +150,7 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ["/build/", "/node_modules/"],
+  testPathIgnorePatterns: ["/dist/", "/node_modules/"],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
