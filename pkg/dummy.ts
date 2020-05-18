5d92d913115bd97a0612eb758ca306fb11034bd1
Andreas Thomas <dev@chronark.com>
2020-05-18 12:12:51 +0200

    Refactor build steps
    The new build pipeline does the following:
    1. Compile all code to javascript
    2. Create a base image with only production dependencies installed
    3. Build service images by copying the dependencies from #2 and
       necessary code from #1.

---
+++ pkg/dummy.ts
@@ -0,0 +1,10 @@
+// This is here so the directory is not empty.
+// Otherwise typescript will not generate a dist/pkg directory and the build will fail.
+// Once there is some actual code in this folder you can remove this file.
+
+/**
+ * Dummy function, see file header.
+ */
+const helloWorld = (): void => {
+    console.log("Hello World")
+}
