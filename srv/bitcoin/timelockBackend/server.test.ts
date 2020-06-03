3c03c769254b723583f139ac78d34972ddb12ff7
Andreas Thomas <dev@chronark.com>
2020-06-03 22:52:09 +0200

    Add multiple test scenarios for the hexTransaction
    - no hex
    - no validAfterBlockHeight
    - neither
    - success

--- srv/bitcoin/timelockBackend/server.test.ts
+++ srv/bitcoin/timelockBackend/server.test.ts
@@ -1,5 +1,13 @@
 import server from "./server"
 import supertest from "supertest"
+import mongoose, { Mongoose } from "mongoose"
+import HexTransaction from "./HexTransaction.model"
+
+jest.mock("./HexTransaction.model")
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
 
 describe("Test API Endpoint /", () => {
   it("should reply to GET with default message", async () => {
@@ -9,22 +17,38 @@ describe("Test API Endpoint /", () => {
   })
 })
 
-
 describe("Test API Endpoint /timelocks", () => {
-    it("should reply to GET with 404", async () => {
-      const response = await supertest(server).get("/timelocks")
-      expect(response.status).toBe(404)
-    })
-    it("should reply to POST with 400 if the request is not valid", async () => {
-        const response = await supertest(server).post("/timelocks")
-        expect(response.status).toBe(400)
-        expect(response.body).toBe("Missing keys: `hex` or `validAfterBlockheight`")
+  it("should reply to GET with 404", async () => {
+    const response = await supertest(server).get("/timelocks")
+    expect(response.status).toBe(404)
+  })
 
-      })
-      it("should reply to POST with 200 if the request is valid", async () => {
-        const response = await supertest(server).post("/timelocks").send({ hex: "02000000SOMETHING", validAfterBlockHeight: 1 })
-        expect(response.status).toBe(200)
-        expect(response.body).toBe("Successfully added transaction hex to database")
-      })
-    })
-  
+  it("should reply to POST with 400 if the request is missing the `hex` key", async () => {
+    const response = await supertest(server).post("/timelocks").send({ validAfterBlockHeight: 1 })
+    expect(response.status).toBe(400)
+    expect(response.body.success).toBe(false)
+    expect(response.body.message).toBe("Missing hex")
+  })
+  it("should reply to POST with 400 if the request is missing the `validAfterBlockHeight` key", async () => {
+    const response = await supertest(server).post("/timelocks").send({ hex: "02000000SOMETHING" })
+    expect(response.status).toBe(400)
+    expect(response.body.success).toBe(false)
+    expect(response.body.message).toBe("Missing validAfterBlockHeight")
+  })
+  it("should reply to POST with 400 if the request is missing both keys", async () => {
+    const response = await supertest(server).post("/timelocks")
+    expect(response.status).toBe(400)
+    expect(response.body.success).toBe(false)
+    expect(response.body.message).toBe("Missing hex")
+  })
+  it("should reply to POST with 200 if the request is valid", async () => {
+    const payload = { hex: "02000000SOMETHING", validAfterBlockHeight: 1 }
+
+    const response = await supertest(server).post("/timelocks").send(payload)
+    expect(response.status).toBe(200)
+    expect(response.body.success).toBe(true)
+    expect(response.body.message).toBe("Successfully added transaction hex to database")
+
+    expect(HexTransaction).toBeCalledWith(payload)
+  })
+})
