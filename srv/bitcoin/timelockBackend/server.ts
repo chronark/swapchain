8f776122852b23aebcee08c9a8e75fce66ff68e0
Andreas Thomas <dev@chronark.com>
2020-06-03 22:53:50 +0200

    Save a transaction hex into the database
    Simple error checks whether the hex and the validAfterBlockHeight are
    set.
    Then saves it in the mongodb, which is not set up yet.

--- srv/bitcoin/timelockBackend/server.ts
+++ srv/bitcoin/timelockBackend/server.ts
@@ -1,5 +1,8 @@
 import express from "express"
 
+import HexTransaction from "./HexTransaction.model"
+import { validateLocaleAndSetLanguage } from "typescript"
+
 const server = express()
 
 server.use(express.json())
@@ -7,5 +10,30 @@ server.use(express.json())
 server.get("/", (req: express.Request, res: express.Response): void => {
   res.json({ message: "Hello World" })
 })
+
+server.post(
+  "/timelocks",
+  async (req: express.Request, res: express.Response): Promise<void> => {
+    const { hex, validAfterBlockHeight } = req.body
+    if (typeof hex === "undefined" || hex === "") {
+      res.status(400)
+      res.json({ success: false, message: "Missing hex" })
+      return
+    }
+    if (typeof validAfterBlockHeight === "undefined" || validAfterBlockHeight === "") {
+      res.status(400)
+      res.json({ success: false, message: "Missing validAfterBlockHeight" })
+      return
+    }
+
+    const tx = new HexTransaction({ hex: hex, validAfterBlockHeight: validAfterBlockHeight })
+    await tx.save()
+
+    res.json({
+      success: true,
+      message: "Successfully added transaction hex to database",
+    })
+  },
+)
 
 export default server
