dddbc6ba2269f5dbf48a15edfd328f4870077d00
Andreas Thomas <dev@chronark.com>
2020-06-03 22:16:59 +0200

    Add tests for successful and denied requests

--- srv/bitcoin/timelockBackend/server.test.ts
+++ srv/bitcoin/timelockBackend/server.test.ts
@@ -8,3 +8,23 @@ describe("Test API Endpoint /", () => {
     expect(response.body).toEqual({ message: "Hello World" })
   })
 })
+
+
+describe("Test API Endpoint /timelocks", () => {
+    it("should reply to GET with 404", async () => {
+      const response = await supertest(server).get("/timelocks")
+      expect(response.status).toBe(404)
+    })
+    it("should reply to POST with 400 if the request is not valid", async () => {
+        const response = await supertest(server).post("/timelocks")
+        expect(response.status).toBe(400)
+        expect(response.body).toBe("Missing keys: `hex` or `validAfterBlockheight`")
+
+      })
+      it("should reply to POST with 200 if the request is valid", async () => {
+        const response = await supertest(server).post("/timelocks").send({ hex: "02000000SOMETHING", validAfterBlockHeight: 1 })
+        expect(response.status).toBe(200)
+        expect(response.body).toBe("Successfully added transaction hex to database")
+      })
+    })
+  
