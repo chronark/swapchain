b649eaf95131c4d55fba0da14d456abd4a8f6f78
Andreas Thomas <dev@chronark.com>
2020-06-04 12:55:18 +0200

    Add test for duplicate entries
    We don't want the same hexTransaction twice.

--- srv/bitcoin/timelockBackend/server.test.ts
+++ srv/bitcoin/timelockBackend/server.test.ts
@@ -41,7 +41,17 @@ describe("Test API Endpoint /timelocks", () => {
     expect(response.body.success).toBe(false)
     expect(response.body.message).toBe("Missing hex")
   })
+
+  it("should reply to POST with 400 if the transactionHex is already in the database", async () => {
+    const response = await supertest(server).post("/timelocks")
+    expect(response.status).toBe(400)
+    expect(response.body.success).toBe(false)
+    expect(response.body.message).toBe("Missing hex")
+  })
+
   it("should reply to POST with 200 if the request is valid", async () => {
+    HexTransaction.prototype.save = jest.fn().mockImplementation(() => {})
+
     const payload = { hex: "02000000SOMETHING", validAfterBlockHeight: 1 }
 
     const response = await supertest(server).post("/timelocks").send(payload)
