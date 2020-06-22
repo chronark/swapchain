d0dd587957891549244fe64f1d91ccc5ac2eb38f
Andreas Thomas <dev@chronark.com>
2020-06-22 14:12:56 +0200

    Change row border from bottom to top

--- srv/web/src/components/orderbook/Row.tsx
+++ srv/web/src/components/orderbook/Row.tsx
@@ -13,7 +13,7 @@ export const Row = (props: Props) => {
 
     return (
         <tr onClick={() => onClick(order)} className={`border-l-2 border-transparent hover:bg-gray-100 hover:border-gray-400 ${order.selected ? "bg-gray-100 border-l-4 border-gray-400" : ""}`}>
-            <td className="table-cell px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+            <td className="table-cell px-6 py-4 whitespace-no-wrap border-t border-gray-200">
                 <div className="flex flex-col text-sm leading-5">
                     <span className="text-gray-700">
                         {order.created.toLocaleTimeString()}
@@ -23,12 +23,12 @@ export const Row = (props: Props) => {
                     </span>
                 </div>
             </td>
-            <td className="px-6 py-4 text-left whitespace-no-wrap border-b border-gray-200">
+            <td className="px-6 py-4 text-left whitespace-no-wrap border-t border-gray-200">
                 <span className={`px-2 text-xs uppercase font-semibold rounded bg-${order.status.color}-100 text-${order.status.color}-800`}>
                     {order.status.label}
                 </span>
             </td>
-            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+            <td className="px-6 py-4 whitespace-no-wrap border-t border-gray-200">
                 <div className="flex items-center">
                     <span className="inline-flex px-2 text-xs font-semibold uppercase bg-gray-300 rounded ">
                         {order.give.asset}
@@ -39,7 +39,7 @@ export const Row = (props: Props) => {
                     </span>
                 </div>
             </td>
-            <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+            <td className="px-6 py-4 whitespace-no-wrap border-t border-gray-200">
                 <div className="flex items-center">
                     <span className="flex items-center ml-2 leading-5 text-gray-500" >
                         {order.exchangeRate.toFixed(5)}
