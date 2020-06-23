eaf6a9599798201b42654f8de1d5d8dcb057ad72
madsjordt <mads.jordt@fau.de>
2020-06-23 21:47:36 +0200

    Add snapshot for testing purposes

--- srv/web/src/components/orderbook/Orderbook.test.tsx
+++ srv/web/src/components/orderbook/Orderbook.test.tsx
@@ -1,37 +1,34 @@
-import React from 'react';
-import Orderbook, {Order} from './Orderbook';
-import renderer from 'react-test-renderer';
+import React from "react"
+import Orderbook, { Order } from "./Orderbook"
+import renderer from "react-test-renderer"
 
 jest.spyOn(Math, "random").mockImplementation(() => 1)
 jest.spyOn(Date.prototype, "toLocaleString").mockImplementation(() => "IAmADate")
 jest.spyOn(Date.prototype, "toLocaleTimeString").mockImplementation(() => "IAmADate")
 
+it("renders correctly", () => {
+  const order: Order = {
+    selected: true,
+    addressHash: "asdf",
+    created: new Date(),
+    validUntil: new Date(),
+    status: { label: "label", color: "red" },
+    give: {
+      asset: "BTC",
+      value: 1,
+    },
+    exchangeRate: 1,
+    receive: {
+      asset: "BTS",
+      value: 1,
+    },
+  }
 
-it('renders correctly', () => {
-    const order: Order = {
-        selected: true,
-        addressHash: "asdf",
-        created: new Date(),
-        validUntil: new Date(),
-        status: { label: "label", color: "red" },
-        give: {
-            asset: "BTC",
-            value: 1
-        },
-        exchangeRate: 1,
-        receive: {
-            asset: "BTS",
-            value: 1,
-        }
-    }
+  const orders = []
+  for (let i = 0; i < 10; i++) {
+    orders.push(order)
+  }
 
-    const orders = []
-    for (let i = 0; i < 10; i++) {
-        orders.push(order)
-    }
-
-    const tree = renderer
-        .create(<Orderbook orders={orders}></Orderbook>)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-});
+  const tree = renderer.create(<Orderbook orders={orders}></Orderbook>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
