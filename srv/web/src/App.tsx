b8cd1e19a025078affb41d6c78a027f814dae245
JuliaSte <julia_herford@hotmail.de>
2020-06-24 11:32:58 +0200

    Change responsiveness
    Change the way to make the website responsive
    
    Co-authored-by: JoabDavid <joabdavid96@gmail.com>

--- srv/web/src/App.tsx
+++ srv/web/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Orderbook from "./components/orderbook/Orderbook"
 import { orderFactory} from "./components/orderbook/Orderbook"
-import StartingPage from './components/StartingPage';
-import AboutProject from './components/AboutProject';
-import ACCS from './components/ACCS'; 
+import StartingPage from './components/startingpages/StartingPage';
+import AboutProject from './components/startingpages/AboutProject';
+import ACCS from './components/startingpages/ACCS'; 
 
 
 
@@ -12,8 +12,8 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <StartingPage></StartingPage>
-       <AboutProject></AboutProject>
-       <ACCS></ACCS>
+      <ACCS></ACCS>
+      <AboutProject></AboutProject>
      <div className="p-20">
        
 
