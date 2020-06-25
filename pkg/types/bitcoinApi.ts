dd99b0a37558857ee506c7e62a5439587cd78c50
Andreas Thomas <dev@chronark.com>
2020-06-25 19:45:38 +0200

    Clean up and add remaining function declarations

--- pkg/types/bitcoinApi.ts
+++ pkg/types/bitcoinApi.ts
@@ -1,29 +1,17 @@
-/*
- Push transaction
- get current blockheight
- get block at height
-    - so we can calculate timedifference between blocks
- get transaction by id
-    - for vout
-    - for balance
-
-*/
+import { Payment } from "bitcoinjs-lib/types/payments"
 
 /**
  * This interface defines the data required for the HTLC creation on the bitcoin blockchain.
- * You are welcome to implement this yourself and pass it to the HTLC class .
- *
- * For development we are using https://blockstream.info/api to query data and push transactions.
+ * You are welcome to implement this yourself and pass it to the HTLC class.
  *
  * The return objects are explicitly stated here as these are the only values we require.
  * Anything else returned from your api of choice can be ignored.
  *
- * @export
  * @interface BitcoinAPI
  */
 export interface BitcoinAPI {
   /**
-   * Return useful information about the last block.
+   * Return height and timestamp of the last block.
    * Used for timelock calculations.
    */
   getLastBlock: () => Promise<{ height: number; timestamp: number }>
@@ -34,6 +22,7 @@ export interface BitcoinAPI {
    */
   getTimestampAtHeight: (n: number) => Promise<number>
 
+  // TODO: Do we need this?
   /**
    * Return the transaction details.
    * Used to get
@@ -47,4 +36,22 @@ export interface BitcoinAPI {
    * Throws on failure
    */
   pushTX: (transactionHex: string) => Promise<string>
+
+  // TODO: check for logic [ in <-> out ]
+  /**
+   * Returns either the last incoming transaction or outgoing transaction of an address.
+   *
+   * @param address - Any bitcoin address.
+   * @param out - Search for an incoming or outgoing transaction.
+   * @returns Amount of the transaction and its ID.
+   */
+  getAmountFromLastTransaction: (address: string, out: boolean) => Promise<{ amount: number; txID: string }>
+
+  /**
+   * Return the vout and value of the transaction.
+   *
+   * @param transactionID - ID of any bitcoin transaction.
+   * @returns vout and value
+   */
+  getOutput: (transactionID: string) => Promise<{ vout: number; value: number }>
 }
