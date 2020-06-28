216682e3817ca8a19e076cea456f550acd3f65de
webersni <nicolas.webersinke@outlook.com>
2020-06-28 12:54:07 +0200

    Change bitcoin address type to segwit

--- pkg/address/address.test.ts
+++ pkg/address/address.test.ts
@@ -1,25 +1,45 @@
 import * as bitcoin from "bitcoinjs-lib"
-import { getBTCAddress } from "./address"
+import { PrivateKey } from "bitsharesjs"
+import { getBTCAddress, getBTSAddress } from "./address"
+
 describe("BTC address generator", () => {
-  const addressPair = getBTCAddress("bitcoin")
+  const addressPair = getBTCAddress("mainnet")
   const addressPair2 = getBTCAddress("testnet")
   const addressPair3 = getBTCAddress("regtest")
 
-  it("should return an address with 1, n or m as first char", () => {
-    expect(addressPair.address.startsWith("1")).toBe(true)
-    expect(addressPair2.address.startsWith("m") || addressPair2.address.startsWith("n")).toBe(true)
-    expect(addressPair3.address.startsWith("m") || addressPair3.address.startsWith("n")).toBe(true)
+  it("should return an address starting with bc1, tb1 or bcrt1", () => {
+    expect(addressPair.address.startsWith("bc1")).toBe(true)
+    expect(addressPair2.address.startsWith("tb1")).toBe(true)
+    expect(addressPair3.address.startsWith("bcrt1")).toBe(true)
   })
   it("should be possible to calculate address with privateKey", () => {
     const keyPair = bitcoin.ECPair.fromWIF(addressPair.privateKey)
-    const address = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey, network: bitcoin.networks.bitcoin }).address
-    expect(address).toBe(addressPair.address)
+    const address = bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey, network: bitcoin.networks.bitcoin }).address
+    expect(addressPair.address).toBe(address)
   })
   it("should return an error with an invalid network", () => {
     expect(() => getBTCAddress("XYZ")).toThrow("Invalid network name. Choose bitcoin, testnet or regtest.")
   })
   it("should return an error if address is undefined", () => {
-    jest.spyOn(bitcoin.payments, "p2pkh").mockImplementation((): bitcoin.payments.Payment => ({}))
+    jest.spyOn(bitcoin.payments, "p2wpkh").mockImplementation((): bitcoin.payments.Payment => ({}))
     expect(() => getBTCAddress("testnet")).toThrow("Unknown error during address generation.")
+  })
+})
+
+describe("BTS address generator", () => {
+  const addressPair = getBTSAddress("mainnet")
+  const addressPair2 = getBTSAddress("testnet")
+
+  it("should return an address starting with the corresponding network prefix", () => {
+    expect(addressPair.address.startsWith("BTS")).toBe(true)
+    expect(addressPair2.address.startsWith("TEST")).toBe(true)
+  })
+  it("should be possible to calculate address with privateKey", () => {
+    const privateKey = PrivateKey.fromWif(addressPair2.privateKey)
+    const address = privateKey.toPublicKey().toAddressString()
+    expect(address).toBe(addressPair2.address)
+  })
+  it("should return an error with an invalid network", () => {
+    expect(() => getBTSAddress("XYZ")).toThrow("Invalid network name. Choose bitshares or testnet.")
   })
 })
