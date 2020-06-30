260eccd2e62fb11114219d1a1192185174c22d37
Andreas Thomas <dev@chronark.com>
2020-06-30 13:30:05 +0200

    Finish refactoring of the btcHTLC class
    We are now using the new btcAPI class and removed all node-fetch calls.
    
    Co-authored-by: webersni <nicolas.webersinke@outlook.com>

--- pkg/bitcoin/htlc/btcHTLC.test.ts
+++ pkg/bitcoin/htlc/btcHTLC.test.ts
@@ -2,7 +2,6 @@ import BitcoinHTLC from "./btcHTLC"
 import * as bitcoin from "bitcoinjs-lib"
 import { mocked } from "ts-jest/utils"
 import BlockStream from "../api/blockstream"
-import axios from "axios"
 jest.mock("axios")
 jest.mock("../api/blockstream")
 
@@ -12,13 +11,30 @@ const bob = bitcoin.ECPair.fromWIF("cNaEjitvA19JZxWAFyCFMsm16TvGEmVAW3AkPnVr8E9v
 const bobCompressed = bitcoin.ECPair.fromPrivateKey(bob.privateKey!, { compressed: true })
 const aliceCompressed = bitcoin.ECPair.fromPrivateKey(alice.privateKey!, { compressed: true })
 
+const getBlockstreamMock = () => {
+  return {
+    getLastBlock: jest
+      .spyOn(BlockStream.prototype, "getLastBlock")
+      .mockResolvedValue({ height: 1000, timestamp: 5000 }), // TODO: change the values
+    getTimestampAtHeight: jest.spyOn(BlockStream.prototype, "getTimestampAtHeight").mockResolvedValue(6000),
+    pushTX: jest
+      .spyOn(BlockStream.prototype, "pushTX")
+      .mockResolvedValue("4a2d7af13070119a0b8a36082df0a03c17e60059250598b100260b0a6949a9ee"),
+    getPreimageFromLastTransactioin: jest
+      .spyOn(BlockStream.prototype, "getPreimageFromLastTransaction")
+      .mockResolvedValue("amos"),
+    getValueFromLastTransaction: jest
+      .spyOn(BlockStream.prototype, "getValueFromLastTransaction")
+      .mockResolvedValue({ value: 2, txID: "txID" }),
+    getOutput: jest.spyOn(BlockStream.prototype, "getOutput").mockResolvedValue({ vout: 1, value: 1_000_000 }),
+    getBlockHeight: jest.spyOn(BlockStream.prototype, "getBlockHeight").mockResolvedValue(1_000_000),
+  }
+}
+
 const secret = {
   preimage: "TOPSECRET",
   hash: bitcoin.crypto.sha256(Buffer.from("TOPSECRET")),
 }
-
-const toHex = jest.fn()
-
 beforeEach(() => {
   jest.resetAllMocks()
 
@@ -29,7 +45,7 @@ beforeEach(() => {
   bitcoin.Psbt.prototype.validateSignaturesOfInput = jest.fn().mockReturnValue(bitcoin.Psbt.prototype)
   bitcoin.Psbt.prototype.finalizeInput = jest.fn().mockReturnValue(bitcoin.Psbt.prototype)
   bitcoin.Psbt.prototype.finalizeAllInputs = jest.fn().mockReturnValue(bitcoin.Psbt.prototype)
-  bitcoin.Psbt.prototype.extractTransaction = jest.fn().mockReturnValue({ toHex })
+  bitcoin.Psbt.prototype.extractTransaction = jest.fn().mockReturnValue({ toHex: () => "hex" })
 
   bitcoin.payments.p2wsh = jest.fn().mockReturnValue({
     network: {
@@ -54,82 +70,65 @@ beforeEach(() => {
     name: "NAME",
   })
 })
+describe("btcHTLC", () => {
+  describe("create()", () => {
+    const testCases = [
+      {
+        value: 2_000,
+        sequence: 1,
+      },
+      {
+        value: 1_000,
+        sequence: 10,
+      },
+    ]
+    testCases.forEach((tc) => {
+      it("create method calls methods where bob redeems the HTLC", async () => {
+        const blockStreamMock = getBlockstreamMock()
 
-beforeAll(() => {
-  //   fetchMock.get("begin:https://api.blockcypher.com/v1/btc/test3/txs/", {
-  //     hash: "4a2d7af13070119a0b8a36082df0a03c17e60059250598b100260b0a6949a9ee",
-  //     hex:
-  //       "02000000018dce163d3412f4253fe1c0fe18b1eccd9b38c96483458822b75792131f1bbf7c000000006b483045022100912cd98bf9388bc18092ada6f1c7dbe043fd6019cc54c5c4ed77b677ac5774830220437f9a7293ab6edb89ecc41c121ae8d1dbbfcc7ed060c0fa5c1e3cb0d337158601210298afe552bd28e6f874c827d8de7291dffa29a87d8b03a1d74e4a0a14c8ca7ad9ffffffff02905f0100000000001976a91424404a3db4a9cf9832cd06c89d089908b8b8e23d88ace80300000000000017a9144029f488d4d407b6c19a15d97bf78bcac8a8142a8700000000",
-  //     outputs: [
-  //       {
-  //         value: 100_000,
-  //         script: "76a91424404a3db4a9cf9832cd06c89d089908b8b8e23d88ac",
-  //         addresses: ["tb1qy3qy50d5488esvkdqmyf6zyepzut3c3ap748sk"],
-  //         /* eslint-disable-next-line @typescript-eslint/camelcase */
-  //         script_type: "pay-to-pubkey-hash",
-  //       },
-  //       {
-  //         value: 1_000,
-  //         script: "76a91403f338e0f898ef40f1eb6734dc1f5aa72a4161f888ac",
-  //         addresses: ["mfsqh18UT3XjpJ8yVeiM7YX1mLxq5REG9d"],
-  //         /* eslint-disable-next-line @typescript-eslint/camelcase */
-  //         script_type: "pay-to-pubkey-hash",
-  //       },
-  //     ],
-  //   })
-})
+        const htlc = new BitcoinHTLC("testnet", aliceCompressed, bobCompressed, BlockStream)
 
-const testCases = [
-  {
-    value: 2_000,
-    sequence: 1,
-  },
-  // {
-  //   value: 1_000,
-  //   sequence: 10,
-  // },
-]
-testCases.forEach((tc) => {
-  it("create method calls methods where bob redeems the HTLC", async () => {
-    const blockStreamMock = {
-      getLastBlock: jest
-        .spyOn(BlockStream.prototype, "getLastBlock")
-        .mockResolvedValue({ height: 1000, timestamp: 5000 }), // TODO: change the values
-      getTimestampAtHeight: jest.spyOn(BlockStream.prototype, "getTimestampAtHeight").mockResolvedValue(6000),
-      pushTX: jest
-        .spyOn(BlockStream.prototype, "pushTX")
-        .mockResolvedValue("4a2d7af13070119a0b8a36082df0a03c17e60059250598b100260b0a6949a9ee"),
-      getPreimageFromLastTransactioin: jest
-        .spyOn(BlockStream.prototype, "getPreimageFromLastTransaction")
-        .mockResolvedValue("amos"),
-      getValueFromLastTransaction: jest
-        .spyOn(BlockStream.prototype, "getValueFromLastTransaction")
-        .mockResolvedValue({ value: 2, txID: "txID" }),
-      getOutput: jest.spyOn(BlockStream.prototype, "getOutput").mockResolvedValue({ vout: 1, value: 1_000_000 }),
-      getBlockHeight: jest.spyOn(BlockStream.prototype, "getBlockHeight").mockResolvedValue(1_000_000),
-    }
+        const htlcConfig = {
+          transactionID: "4a2d7af13070119a0b8a36082df0a03c17e60059250598b100260b0a6949a9ee",
+          amount: tc.value,
+          sequence: tc.sequence,
+          hash: secret.hash,
+        }
 
-    const htlc = new BitcoinHTLC("testnet", aliceCompressed, bobCompressed, BlockStream)
+        await htlc.create(htlcConfig)
 
-    const htlcConfig = {
-      transactionID: "4a2d7af13070119a0b8a36082df0a03c17e60059250598b100260b0a6949a9ee",
-      amount: tc.value,
-      sequence: tc.sequence,
-      hash: secret.hash,
-    }
+        expect(bitcoin.payments.p2wsh).toHaveBeenCalledTimes(1)
 
+        expect(bitcoin.Psbt.prototype.signInput).toHaveBeenCalledTimes(2)
+        expect(bitcoin.Psbt.prototype.validateSignaturesOfInput).toHaveBeenCalledTimes(1)
+        expect(bitcoin.Psbt.prototype.finalizeInput).toHaveBeenCalledTimes(1)
+        expect(bitcoin.Psbt.prototype.finalizeAllInputs).toHaveBeenCalledTimes(1)
+        expect(bitcoin.Psbt.prototype.extractTransaction).toHaveBeenCalledTimes(2)
+        expect(blockStreamMock.pushTX).toHaveBeenCalledTimes(1)
+      })
+    })
+  })
 
+  describe("redeem()", () => {
+    it("calls methods to redeem the HTLC", async () => {
+      bitcoin.Psbt.prototype.addInput = jest.fn().mockReturnValue(bitcoin.Psbt.prototype)
+      bitcoin.Psbt.prototype.addOutput = jest.fn().mockReturnValue(bitcoin.Psbt.prototype)
 
+      const blockStreamMock = getBlockstreamMock()
 
-    await htlc.create(htlcConfig)
-    expect(bitcoin.payments.p2wsh).toHaveBeenCalledTimes(1)
+      const htlc = new BitcoinHTLC("testnet", aliceCompressed, bobCompressed, BlockStream)
 
-    expect(bitcoin.Psbt.prototype.signInput).toHaveBeenCalledTimes(2)
-    expect(bitcoin.Psbt.prototype.validateSignaturesOfInput).toHaveBeenCalledTimes(1)
-    expect(bitcoin.Psbt.prototype.finalizeInput).toHaveBeenCalledTimes(1)
-    expect(bitcoin.Psbt.prototype.finalizeAllInputs).toHaveBeenCalledTimes(1)
-    expect(bitcoin.Psbt.prototype.extractTransaction).toHaveBeenCalledTimes(2)
-    expect(blockStreamMock.pushTX).toHaveBeenCalledTimes(1)
-    expect(toHex).toHaveBeenCalledTimes(2)
+      const p2wsh = htlc.getP2WSH(secret.hash, 1)
+
+      const res = await htlc.redeem(p2wsh, secret)
+
+      expect(res).toBe(0)
+      expect(bitcoin.payments.p2wsh).toHaveBeenCalledTimes(1)
+
+      expect(bitcoin.Psbt.prototype.signInput).toHaveBeenCalledTimes(1)
+      expect(bitcoin.Psbt.prototype.finalizeInput).toHaveBeenCalledTimes(1)
+      expect(bitcoin.Psbt.prototype.extractTransaction).toHaveBeenCalledTimes(1)
+      expect(blockStreamMock.pushTX).toHaveBeenCalledTimes(1)
+    })
   })
 })
--- pkg/bitcoin/htlc/btcHTLC.ts
+++ pkg/bitcoin/htlc/btcHTLC.ts
@@ -3,8 +3,6 @@ import { PsbtInput } from "bip174/src/lib/interfaces"
 import { witnessStackToScriptWitness } from "./witnessStack"
 import { Secret } from "../../../pkg/secret/secret"
 import { BitcoinAPI, BitcoinAPIConstructor } from "../../types/bitcoinApi"
-import { getOutputFileNames } from "typescript"
-import { p2sh } from "bitcoinjs-lib/types/payments"
 
 /**
  * Contains all necessary information to create an HTLC on Bitcoin blockchain
@@ -75,7 +73,7 @@ export default class BitcoinHTLC {
 
   /**
    * Get block height of funding transaction
-   * 
+   *
    * @returns Blockheight of funding transaction or undefined if block is not mined/broadcasted yet.
    * @memberof BitcoinHTLC
    */
@@ -243,57 +241,53 @@ export default class BitcoinHTLC {
   private async sendToP2WSHAddress(p2wsh: bitcoin.Payment, transactionID: string, amount: number): Promise<string> {
     const p2wpkFromSender = this.getWitnessPublicKeyHash(this.sender)
     // Value is the current balance after the transaction
-    const { vout, value } = await this.bitcoinAPI.getOutput(transactionID, p2wpkFromSender.address!)
-    
+    const x = await this.bitcoinAPI.getOutput(transactionID, p2wpkFromSender.address!)
+    const { vout, value } = x
     const amountToKeep = value - amount
-    console.warn({amountToKeep})
-    
     // If output of the transaction ID given does not have sufficient value/balance
     if (amountToKeep < 0) {
       return ""
     }
-    console.warn({amount})
     this.amountAfterFees = amount - 200 // TODO: Add option for higher and lower fees
-    
+
     if (this.amountAfterFees <= 0) {
-      //TODO: Change value to actual fees
-      throw new Error("You are trying to send less money than the fees to transfer it, please use a value higher than 200")
+      // TODO: Change value to actual fees
+      throw new Error(
+        "You are trying to send less money than the fees to transfer it, please use a value higher than 200",
+      )
     }
 
-
     const outputs =
-    amountToKeep > 0
-    ? [
+      amountToKeep > 0
+        ? [
             {
               address: p2wpkFromSender.address!,
               value: amountToKeep,
             },
             {
               address: p2wsh.address!,
-              value: this.amountAfterFees, // FIXME:
+              value: this.amountAfterFees,
             },
           ]
         : [
-          {
-            address: p2wsh.address!,
-            value: this.amountAfterFees,
-          },
-        ]
-        console.warn({vout, value})
-        const psbt = new bitcoin.Psbt({ network: this.network })
-        .setVersion(2)
-        .addInput({
-          hash: transactionID,
-          index: vout,
-          witnessUtxo: {
-            script: p2wpkFromSender.output!,
-            value,
-          },
-        })
-        .addOutputs(outputs)
-        .signInput(0, this.sender)
-        
-        console.error("BBBBBBBBBBBB")
+            {
+              address: p2wsh.address!,
+              value: this.amountAfterFees,
+            },
+          ]
+    const psbt = new bitcoin.Psbt({ network: this.network })
+      .setVersion(2)
+      .addInput({
+        hash: transactionID,
+        index: vout,
+        witnessUtxo: {
+          script: p2wpkFromSender.output!,
+          value,
+        },
+      })
+      .addOutputs(outputs)
+      .signInput(0, this.sender)
+
     psbt.validateSignaturesOfInput(0)
     psbt.finalizeAllInputs()
     return psbt.extractTransaction().toHex()
@@ -312,10 +306,10 @@ export default class BitcoinHTLC {
     this.preimage = secret.preimage!
 
     const { value, txID } = await this.bitcoinAPI.getValueFromLastTransaction(p2wsh.address!)
-
     // TODO: Check if amount is enough!
     this.amountAfterFees = value - 200
     const redeemHex = await this.getRedeemHex(txID, p2wsh)
+
     const redeemTransaction = await this.bitcoinAPI.pushTX(redeemHex)
 
     if (!redeemTransaction) {
@@ -340,8 +334,7 @@ export default class BitcoinHTLC {
     const p2wsh = this.getP2WSH(hash, sequence)
     // Funding
     const p2wpkHex = await this.sendToP2WSHAddress(p2wsh, transactionID, amount)
-    
-    console.error("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa")
+
     if (!p2wpkHex) {
       // The output of the transaction ID given does not have sufficient balance.
       return 1
@@ -364,6 +357,7 @@ export default class BitcoinHTLC {
     const refundHex = await this.getRefundHex(fundingTransactionID, sequence, p2wsh)
     console.log("refundHex: " + refundHex)
 
+    this.fundingTxBlockHeight = await this.bitcoinAPI.getBlockHeight(fundingTransactionID)
     while (!this.fundingTxBlockHeight) {
       this.fundingTxBlockHeight = await this.bitcoinAPI.getBlockHeight(fundingTransactionID)
       console.warn(`Still waiting for blockheight... Currently at ${this.fundingTxBlockHeight}`)
@@ -432,7 +426,6 @@ export default class BitcoinHTLC {
    */
   private async getRedeemHex(transactionID: string, p2wsh: bitcoin.payments.Payment): Promise<string> {
     const { vout, value } = await this.bitcoinAPI.getOutput(transactionID, p2wsh.address!)
-
     const psbt = new bitcoin.Psbt({ network: this.network })
       .setVersion(2)
       .addInput({
@@ -450,7 +443,6 @@ export default class BitcoinHTLC {
       })
       .signInput(0, this.receiver)
       .finalizeInput(0, this.getFinalScriptsRedeem)
-
     return psbt.extractTransaction().toHex()
   }
 }
