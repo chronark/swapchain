1a9d9b4169b5f38b6899bd4b4725a4d7cb6cdbd2
madsjordt <mads.jordt@fau.de>
2020-07-01 10:11:10 +0200

    Installed dependencies and edited formating

--- pkg/bitcoin/htlc/database/connection.ts
+++ pkg/bitcoin/htlc/database/connection.ts
@@ -1,17 +1,15 @@
-require('dotenv').config(({ path: '/Users/madsjordt/Desktop/Projects/AMOS/swapchain/.env' }))
 import server from "./server"
 import mongoose from "mongoose"
- 
-  const port = process.env.PORT || 8080
-  const mongoPort = process.env.MONGO_PORT || 27017
-  const dbName = process.env.DB_NAME || "test"
-  const URI = process.env.MONGOURI
+require("dotenv").config({ path: "/Users/madsjordt/Desktop/Projects/AMOS/swapchain/.env" })
 
-  mongoose
-    .connect(URI!, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
-        console.log("MongoDB connected")
-        server.listen(port, () => {
-          console.log(`Listening on port ${port}`)
-        })
-      })
- 
+const port = process.env.PORT || 8080
+const mongoPort = process.env.MONGO_PORT || 27017
+const dbName = process.env.DB_NAME || "test"
+const URI = process.env.MONGOURI
+
+mongoose.connect(URI!, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
+  console.log("MongoDB connected")
+  server.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+  })
+})
