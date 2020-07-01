8933585929b8bf2ba98f6f9ee58a29357d252a38
madsjordt <mads.jordt@fau.de>
2020-07-01 12:00:24 +0200

    Fixed .env path and therewith test

--- pkg/bitcoin/htlc/database/connection.ts
+++ pkg/bitcoin/htlc/database/connection.ts
@@ -1,6 +1,6 @@
 import server from "./server"
 import mongoose from "mongoose"
-require("dotenv").config({ path: "swapchain/.env" })
+require("dotenv").config({ path: '.env' })
 
 const port = process.env.PORT || 8080
 const mongoPort = process.env.MONGO_PORT || 27017
