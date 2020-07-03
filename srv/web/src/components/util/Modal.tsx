37d99e1b7adaf67369e600b5c60173298e266eb9
Andreas Thomas <dev@chronark.com>
2020-07-03 11:57:22 +0200

    Format

--- srv/web/src/components/util/Modal.tsx
+++ srv/web/src/components/util/Modal.tsx
@@ -9,22 +9,22 @@ type Props = {
 
 
 export default (props: Props) => {
-  
+
     return (
         <div className={props.open ? "" : "hidden"}>
             <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
             <div className="absolute inset-0 z-10 flex items-center justify-center">
                 <div className="p-12 bg-white rounded-sm">
                     <header className="flex items-center justify-between">
-                    <h1 className="text-lg font-bold text-gray-500">{props.title}</h1>
+                        <h1 className="text-lg font-bold text-gray-500">{props.title}</h1>
                         <X className="w-8 h-8 text-gray-400 duration-200 ease-in-out hover:text-gray-800" onClick={props.close}></X>
                     </header>
                     <div className="flex flex-col items-center justify-around md:flex-row ">
-                    {props.children}
-                </div>
+                        {props.children}
+                    </div>
 
+                </div>
             </div>
-        </div>
         </div>
     )
 }
