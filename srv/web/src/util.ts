36497990851d8c08e52f1416f057ff1f55d56075
webersni <nicolas.webersinke@outlook.com>
2020-07-05 21:35:44 +0200

    Add private to public key converter

--- srv/web/src/util.ts
+++ srv/web/src/util.ts
@@ -1,4 +1,5 @@
 import crypto from "crypto"
+import * as bitcoin from "bitcoinjs-lib"
 
 export const fakeAddress = (length: number) => {
   return hash(Math.random().toString()).substring(0, length) + "..."
@@ -6,4 +7,21 @@ export const fakeAddress = (length: number) => {
 
 export const hash = (s: string): string => {
   return crypto.createHash("SHA256").update(s).digest("hex")
+}
+
+export const getPublicKey = (privateKey: string): string => {
+  if (!privateKey) {
+    return ""
+  }
+  let keyPair: bitcoin.ECPairInterface
+  try {
+    keyPair = bitcoin.ECPair.fromWIF(privateKey, bitcoin.networks.testnet)
+  } catch(err) {
+    return "Invalid private key"
+  }
+
+  const keyPairCompressed = bitcoin.ECPair.fromPrivateKey(keyPair.privateKey!, { compressed: true })
+
+  return keyPairCompressed.publicKey.toString("hex")
+
 }
