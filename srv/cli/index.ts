a37c2c1fd3984102b690694659d44ccacdc3e378
webersni <nicolas.webersinke@outlook.com>
2020-07-07 16:05:57 +0200

    Refactor CLI
    Take the CLI out of ACCS class

---
+++ srv/cli/index.ts
@@ -0,0 +1,40 @@
+import ACCS, { ACCSFields } from "../web/src/accs/accs"
+import figlet from "figlet"
+import { getUserInput } from "./getUserInput"
+
+const argv = process.argv.slice(2)
+
+if (argv.length !== 1) {
+  console.error("Usage: node index.js network")
+  process.exit(1)
+}
+if (argv[0] !== "mainnet" && argv[0] !== "testnet") {
+  console.error("swapchain-cli only supports mainnet and testnet")
+  process.exit(1)
+}
+
+// Print beautiful Swapchain banner and version info
+figlet("Swapchain", (err, banner) => {
+  if (err) {
+    throw new Error(err.toString())
+  }
+  console.log(banner)
+})
+
+new Promise((resolve) => setTimeout(resolve, 500)).then(() => {
+  console.log("")
+  console.log("version: sprint-11-release")
+  console.log("Welcome to swapchain-cli.")
+  console.log("")
+
+  getUserInput().then((fields) => {
+    ACCS.run(fields).then(() => {
+      console.log("Success! Thanks for swapping with Swapchain.")
+      console.log("Bye.")
+      process.exit(0)
+    })
+  }).catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
+})
