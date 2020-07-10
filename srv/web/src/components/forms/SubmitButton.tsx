3675f30378b0b46896d7e79b339692665513f7fa
Andreas Thomas <dev@chronark.com>
2020-07-10 11:36:23 +0200

    Fix TradingPage icons and adjusted button style

--- srv/web/src/components/forms/SubmitButton.tsx
+++ srv/web/src/components/forms/SubmitButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 
 export const SubmitButton = (props: Props) => {
-    const borderColor = "border-" + props.borderColor + "-300"
+    const borderColor = "border-" + props.borderColor + "-400"
   
     return (
         <button type="button" onClick={props.onClick} className={`px-8 py-3 font-semibold text-gray-900 uppercase transition duration-200 ease-in-out bg-white border rounded-sm shadow-sm focus:outline-none hover:border-teal-500 hover:bg-white ${borderColor}`}>
