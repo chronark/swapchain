a21fb5ccd531207683825a31d6e6c82ad0205397
webersni <nicolas.webersinke@outlook.com>
2020-07-14 15:13:48 +0200

    Fix doc strings
    Co-authored-by: chronark <dev@chronark.com>

--- src/pkg/bitshares/api/api.ts
+++ src/pkg/bitshares/api/api.ts
@@ -1,42 +1,68 @@
-import { Apis as btsWebsocketApi } from "bitsharesjs-ws"
-import { ChainStore, ChainConfig, PrivateKey } from "bitsharesjs"
+import { Apis as btsWebsocketApi, ChainConfig } from "bitsharesjs-ws"
+import { ChainStore, PrivateKey } from "bitsharesjs"
 
+/**
+ * API to connect to Bitshares blockchain
+ *
+ * Implemented as a singleton, so there is only one websocket at the same time.
+ * Use getInstance to get an instance and not the constructor.
+ */
 export class BitsharesAPI {
   private static instance: BitsharesAPI
 
-
-
+  /**
+   * Do not use
+   *
+   * Only for testing purposes. If you call the constructor, the websocket will not be open.
+   * Use getInstance instead.
+   */
   public constructor() {}
 
+  /**
+   * Initialize websocket if there is not already one
+   *
+   * @param node - The endpoint URL to connect.
+   * @returns BitsharesAPI instance.
+   */
   public static async getInstance(node: string): Promise<BitsharesAPI> {
     if (!BitsharesAPI.instance) {
       BitsharesAPI.instance = await btsWebsocketApi.instance(node, true).init_promise
       await ChainStore.init(false)
-    } 
+    }
     return BitsharesAPI.instance
   }
 
+  /**
+   * Get account ID
+   *
+   * @param accountName - The account name to fetch the ID for.
+   * @returns The account ID.
+   */
   public async getAccountID(accountName: string): Promise<string> {
     return await btsWebsocketApi.db.get_accounts([accountName]).id
   }
 
-  public async getHistory(receiverID: string): Promise<Record<string, any>[]> {
+  /**
+   * Get account history
+   *
+   * @param accountID - The account ID to fetch the history for.
+   * @returns An array of transaction objects.
+   */
+  public async getHistory(accountID: string): Promise<Record<string, any>[]> {
     const limit = 100
-    return await btsWebsocketApi.history.get_relative_account_history(receiverID, 0, limit, 0)
+    return await btsWebsocketApi.history.get_relative_account_history(accountID, 0, limit, 0)
   }
 
   /**
-   * Get the HTLC BitShares ID.
+   * Get the HTLC BitShares ID
    *
-   * Only for internal purposes.
-   * The HTLC ID is necessary to redeem an HTLC.
-   * Websocket must be open.
-   * TODO: update doc
    * @public
+   * @param senderID - The sender's account ID.
+   * @param receiverID - The receiver's account ID.
    * @param amount - The amount of assets of the HTLC.
    * @param hash - The SHA256 hash of the secret.
-   * @returns HTLC ID of an instance or empty string if no matching HTLC was found.
-   * @memberof BitsharesHTLC
+   * @param timelock - The timelock in seconds or undefined.
+   * @returns HTLC ID of an instance.
    */
   public async getID(
     senderID: string,
@@ -47,17 +73,17 @@ export class BitsharesAPI {
   ): Promise<string> {
     const history = await this.getHistory(receiverID)
     const htlc = history.filter((element: any) => {
-        return (
-          typeof element.result[1] === "string" &&
-          element.op[1].from === senderID &&
-          element.op[1].to === receiverID &&
-          /* eslint-disable-next-line @typescript-eslint/camelcase */
-          element.op[1].preimage_hash[1] === hash.toString("hex") &&
-          element.op[1].amount.amount === amount &&
-          /* eslint-disable-next-line @typescript-eslint/camelcase */
-          // Timelock must be in 20% tolerance range
-          (timelock ? element.op[1].claim_period_seconds >= timelock * 0.8 : true)
-        ) 
+      return (
+        typeof element.result[1] === "string" &&
+        element.op[1].from === senderID &&
+        element.op[1].to === receiverID &&
+        /* eslint-disable-next-line @typescript-eslint/camelcase */
+        element.op[1].preimage_hash[1] === hash.toString("hex") &&
+        element.op[1].amount.amount === amount &&
+        /* eslint-disable-next-line @typescript-eslint/camelcase */
+        // Timelock must be in 20% tolerance range
+        (timelock ? element.op[1].claim_period_seconds >= timelock * 0.8 : true)
+      )
     })
 
     if (htlc.length === 1) {
@@ -66,16 +92,19 @@ export class BitsharesAPI {
 
     throw new Error("Could not find id")
   }
+
   /**
-   *  Derive the account ID from a private key.
+   * Derive the account ID from a private key
+   *
    * @param privateKey - Private key of a bitshares account.
    * @param network - Testnet or mainnet.
+   * @returns The account ID for the private key.
    */
   async toAccountID(privateKey: string, network: string): Promise<string> {
     if (privateKey.length !== 51) {
       throw new Error("Invalid private key length.")
     }
-    
+
     // Set chain IDs to get correct prefix for public key
     if (network === "mainnet") {
       ChainConfig.setChainId("4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8")
@@ -84,13 +113,11 @@ export class BitsharesAPI {
     } else {
       throw new Error("Invalid network name. Choose mainnet or testnet.")
     }
-    console.error("AAAAAAAA")
 
     const publicKey = PrivateKey.fromWif(privateKey).toPublicKey().toString()
 
     /* eslint-disable @typescript-eslint/camelcase */
-    const accounts = await btsWebsocketApi.instance.db.get_key_references([publicKey])
-    console.error({accounts})
+    const accounts = await btsWebsocketApi.db.get_key_references([publicKey])
 
     if (typeof accounts[0][0] === "undefined") {
       throw new Error(`Could not find any accounts for private key ${privateKey} on ${network}`)
@@ -100,7 +127,7 @@ export class BitsharesAPI {
   }
 
   /**
-   *
+   * Get preimage from HTLC on blockchain
    *
    * @param  senderID - ID of the sending account.
    * @param  receiverID - ID of the receiving account.
